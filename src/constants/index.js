f1a419ae2fc76897fa1d305ac22366faa1e8443f
Denis Kukobin <denis.kukobin@hipasus.com>
2024-04-11 16:45:56 +0300

    first

---
+++ src/constants/index.js
@@ -0,0 +1,286 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  wallet,
+  ai,
+  furia,
+  // jobit,
+  // tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend",
+    icon: creator,
+
+    link: "https://t.me/my1ncognito",
+  },
+  {
+    title: "UI/UX",
+    icon: backend,
+    link: "https://t.me/shnaps40",
+  },
+  {
+    title: "App",
+    icon: mobile,
+    link: "https://t.me/edk0p",
+  },
+  {
+    title: "Illustrator",
+    icon: web,
+    link: "https://t.me/mileti_silvestro",
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Creating a DAO",
+    company_name: "Ru Crypto Squad",
+    icon: "",
+    iconBg: "#383E56",
+    date: "March 2023 - ...",
+    points: [
+      "We have created a community of like-minded people.",
+      "The community strongly supported novice NFT collections and artists, which allowed us to gather an association of enthusiastic people.",
+      "The first collection was assembled with NFT membership documents proving membership in the DAO.",
+      "An entertaining and encouraging program was held in the form of various auctions and sweepstakes.",
+    ],
+  },
+  {
+    title: "The second stage is Awareness.",
+    company_name: "",
+    icon: "",
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Aug 2023",
+    points: [
+      "We realized that the community needs to expand and introduced ranks. Now each participant, having proved himself, can climb a step higher.It started to motivate me even more.",
+      "We have been improving our community on the VK platform in every possible way, but at some point we realized that the resources of the site only limit us.",
+      "We tried to fix the situation, the community was also expanding and people who were initially skeptical began to respect us.",
+      "There were a lot of drawings of various NFTs, cryptocurrencies and other gifts. We also sent physical utilities to all participants who bought membership.",
+    ],
+  },
+  {
+    title: "Getting to know the TON Blockchain",
+    company_name: "",
+    icon: "",
+    iconBg: "#383E56",
+    date: "Aug 2023 - Dec 2023",
+    points: [
+      "At some point, one of the participants told the community about the young and underappreciated blockchain. After analyzing the perspective and possibilities, the main cast decides to immerse themselves in TON via Telegram.",
+      "Based on closed and open channels, the main group began to slowly introduce the blockchain to its history. Like-minded people are now starting to join us in TON.",
+      "We are carefully studying the structure and social life of the blockchain in all directions. We are starting to fully immerse ourselves in tokens and projects. This increases the income of the DAO.",
+      "Meanwhile, our first generative collection of Furies was already ready. Each element of which was painted by hand and with love.",
+    ],
+  },
+  {
+    title: "A statement about yourself",
+    company_name: "",
+    icon: "",
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "We realized that we weren't strong enough to shoot yet. Therefore, we prepared the ground and studied the weaknesses of the blockchain.",
+      "During this period, the bull market begins and earnings increase, as does interest in the DAO. People are starting to come to us with requests for admission to the community, but we carefully select everyone.",
+      "The creators of the DAO are finally deciding on future products and directions. The experience becomes enough to determine the course.",
+      "We're here. We are founding a subsidiary company for the development of products inside the RСS TECH blockchain. Immediately, we launched a token that blew up the top among newcomers. The competent work of the DAO to this day allows us to control any drawdown. 100 people held the course while the paper hands were coming out. 100 people as a whole. And We will not back down.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Brilliant madmen! I am glad that I am a part of the team!!! This is my best experience.",
+    name: "Dahanowar",
+    designation: "",
+    company: "",
+    image:
+      "https://sun9-75.userapi.com/impf/c637322/v637322111/4652e/CQbXOUwPnvk.jpg?size=1431x2160&quality=96&sign=9851b8323aabbe3cb93e3d3339adb225&type=album",
+  },
+  {
+    testimonial:
+      "With these guys, I really understood what a DAO is. They taught me how to earn money.",
+    name: "timwp",
+    designation: "",
+    company: "",
+    image:
+      "https://sun9-20.userapi.com/impg/UJKu_HSGFDhEWEUQtF980R8oJ1Jpfnnaz87yVQ/noiGe_YQGVM.jpg?size=1620x2160&quality=95&sign=9293f3d3d1c2ac42081c0adaa0a5a91a&type=album",
+  },
+  {
+    testimonial:
+      "It's been a year with these guys. I am glad that I once became a part of the DAO.",
+    name: "StasFormat",
+    designation: "",
+    company: "",
+    image:
+      "https://sun9-57.userapi.com/impg/o-l7sVLCpx8TmVjNwl6rShQKQqUpOTPqrf3MWA/DIGkayNC11s.jpg?size=1200x1600&quality=96&sign=25807955038eb0176790d45c3ebd5c63&type=album",
+  },
+];
+
+const projects = [
+  {
+    name: "Wallet",
+    description:
+      "A prototype of a non-cathode wallet with the ability to exchange tokens, chat between addresses and premium features.We are not disclosing all killer functions at the moment in order to avoid competition",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "ton api",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Func",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: wallet,
+  },
+  {
+    name: "Social Fi",
+    description:
+      "The first social platform with user encouragement based on AI. Video, images, and text generation is possible inside the platform. Chat between users. and of course, content filtering.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Ai",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Func",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "ton api",
+        color: "green-text-gradient",
+      },
+    ],
+    image: ai,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "NFT collection",
+    description:
+      "We are going to launch the NFT collection after the release of the first bot, every detail of which was painted by our artist. The collection will be linked to the RCS token and will have its own utilities.",
+    tags: [
+      {
+        name: "Func",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Hand-made",
+        color: "green-text-gradient",
+      },
+      {
+        name: "painting",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: furia,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
